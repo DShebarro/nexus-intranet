aeed4c91a7c7027608fd6c79f550883ac18405ca
dev-dshebarro <dev.davyshebarro26@gmail.com>
2026-06-08 16:43:45 -0400

    feat(routes): register all new page and API routes in front controller

--- public/index.php
+++ public/index.php
@@ -16,10 +16,24 @@ $request = new \App\Core\Request();
 $router->add('GET', '/',          'DashboardController@index');
 $router->add('GET', '/dashboard', 'DashboardController@index');
 $router->add('GET', '/tasks',     'TaskController@index');
+$router->add('GET', '/contracts', 'ContractController@index');
+$router->add('GET', '/sites',     'SiteController@index');
+$router->add('GET', '/logs',      'LogController@index');
+$router->add('GET', '/chat',      'ChatController@index');
 
 // Rotas da API
 $router->add('POST',   '/api/tasks',         'TaskController@store');
+$router->add('PUT',    '/api/tasks/{id}',    'TaskController@update');
 $router->add('PATCH',  '/api/tasks/{id}/move', 'TaskController@move');
 $router->add('DELETE', '/api/tasks/{id}',    'TaskController@destroy');
+$router->add('POST',   '/api/contracts',     'ContractController@store');
+$router->add('PUT',    '/api/contracts/{id}', 'ContractController@update');
+$router->add('DELETE', '/api/contracts/{id}', 'ContractController@destroy');
+$router->add('POST',   '/api/sites',         'SiteController@store');
+$router->add('PUT',    '/api/sites/{id}',    'SiteController@update');
+$router->add('DELETE', '/api/sites/{id}',    'SiteController@destroy');
+$router->add('DELETE', '/api/logs',          'LogController@clear');
+$router->add('GET',    '/api/chats/{slug}/messages', 'ChatController@getMessages');
+$router->add('POST',   '/api/chats/{slug}/messages', 'ChatController@storeMessage');
 
 $router->dispatch($request);
